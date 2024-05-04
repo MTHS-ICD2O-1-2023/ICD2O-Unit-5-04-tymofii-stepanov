7ed2b9e387be2065b91ff1729b3451b9de815138
Tymofii <159185555+tymofii-stepanov@users.noreply.github.com>
2024-05-04 19:08:40 +0000

    Done with program

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Tymofii All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Tymofii
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict";
+
+/**
+* This function checks if the user qualifies for museum discount
+*/
+function checkDiscount() {
+  // input
+  const age = parseInt(document.getElementById('user-age').value);
+  const dayOfWeek = document.getElementById('day-of-week').value;
+
+  // process
+  let shouldGetDiscount = false;
+  if ((age < 18 || age > 65) && (dayOfWeek === 'Tuesday' || dayOfWeek === 'Thursday')) {
+    shouldGetDiscount = true;
+  }
+
+  // output
+  if (shouldGetDiscount) {
+    document.getElementById('discount-result').innerHTML = 'Congratulations! You qualify for a museum discount.';
+  } else {
+    document.getElementById('discount-result').innerHTML = 'Sorry, you do not qualify for a museum discount.';
+  }
+}
